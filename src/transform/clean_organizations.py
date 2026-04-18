9cb6cbbf48235128a6d41498396798ba624a71d0
Verdy <verdywahyudi321@gmail.com>
2026-04-18 20:55:04 +0700

    stop tracking gitignore

--- src/transform/clean_organizations.py
+++ src/transform/clean_organizations.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from src.utils.logger import logger
+from src.utils.helpers import zip_to_str
 
 
 def clean_organizations(df: pd.DataFrame) -> pd.DataFrame:
@@ -13,7 +14,7 @@ def clean_organizations(df: pd.DataFrame) -> pd.DataFrame:
 
     df["name"]  = df["name"].str.title()
     df["address"] = df["address"].fillna("Unknown")
-    df["zip"]   = df["zip"].astype(str).str.zfill(5)
+    df["zip"]   = zip_to_str(df["zip"])
     df["state"] = df["state"].str.upper()
 
     return df[["organization_id", "name", "address", "city", "state", "zip"]]
